c63dc8f7140f6153ab942409d00eb8eca84a67e1
GersonESS <gebhsantos@gmail.com>
2025-05-10 13:22:00 -0300

    Update app.js

--- 01p/app.js
+++ 01p/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 // Conexão com o MongoDB Atlas - URI diretamente no código
-const client = new MongoClient('mongodb+srv://ralf:123@cluster0.a0ntihz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+const client = new MongoClient('mongodb+srv://gebhsantos:A3YG8lXShNUS7FUw@users.vnnwl.mongodb.net/users?retryWrites=true&w=majority&appName=users');
 let produtosCollection;
 
 async function conectar() {
